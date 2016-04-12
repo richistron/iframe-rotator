c89f52f301ab72b63def7c6bcde5badc685f0b51
Ricardo Rivas G <richistron@gmail.com>
2016-04-12 15:27:33 -0500

    alpha

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,45 @@
-/*
- * Url Loader
- * @param element [container]
- * @param urls [array of url]
- * @param delay [seconds delay]
- */
 class __UrlLoader {
+  /*
+   * Url Loader
+   * @param element [container]
+   * @param urls [array of url]
+   * @param delay [seconds delay]
+   */
+  constructor(element, options) {
+    this.position = 0;
+    this.$el = $(element);
+    this.urls = options.urls;
+    this.delay = options.delay * 1000;
+    this.width = options.width;
+    this.height = options.height;
+    this.showUrl();
+  }
 
-  constructor() {
-    console.log('new shit');
+  showUrl() {
+    this.$el.html(`
+      <iframe src="${ this.urls[this.position] }"
+        style="border: 0;
+        width: ${ this.width };
+        height: ${ this.height };">
+      </iframe>
+    `);
+    this.next();
+  }
+
+  next() {
+    this.getPosition();
+    setTimeout(()=>{
+      this.showUrl();
+    }, this.delay);
+  }
+
+  getPosition() {
+    if (this.position >= (this.urls.length - 1)) {
+      this.position = 0;
+    } else {
+      this.position++;
+    }
   }
 
 }
-
 window.UrlLoader =  __UrlLoader;
--- spec/main.js
+++ spec/main.js
@@ -9,4 +9,13 @@ describe('UrlLoader', () => {
     expect(new UrlLoader).toBeDefined();
   });
 
+  it('fake test', ()=> {
+    new UrlLoader('.yep', {
+      width: '1300px',
+      height: '800px',
+      urls: ['http://richistron.com/#blogs', 'http://www.magmalabs.io'],
+      delay: 10
+    });
+  });
+
 });
